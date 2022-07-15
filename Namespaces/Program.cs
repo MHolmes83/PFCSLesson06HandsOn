6a418c6ce5765ff4f835f87c65c111aed3999af4
Morgaan Holmes <morgaan.holmes@my.scitexas.edu>
2022-07-15 19:03:37 -0400

    Namespaces
    Created a new console application and learned how to generate and manipulate namepaces for additional functionality within a C# document.

---
+++ Namespaces/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using aliasName = csharpNamespace.myExample;
+
+class UsingDirectives
+{
+    public static void Main()
+    {
+        aliasName.print();
+    }
+}
+
+namespace csharpNamespace
+{
+    class myExample
+    {
+        public static void print()
+        {
+            Console.WriteLine("Example of using a using directive.");
+        }
+    }
+}
+    
